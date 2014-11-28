11ba95d92da6d6ae3eaa825e18ba55bc62fb7087
daniel-johnson <janiel.dohnson@gmail.com>
2014-11-27 17:48:45 -0800

    Created customers controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @lot = Lot.first
   end
 end
--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,3 +1,6 @@
 class Lot < ActiveRecord::Base
-has_many :parking_events
+  has_many :parking_events
+
+  geocoded_by :address
+  after_validation :geocode
 end
